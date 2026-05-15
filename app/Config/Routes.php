8f0357606818b9eccc15f443948c275aa00cb853
hovnous1233 <vlhnatehvn@gmail.com>
2026-05-15 12:49:37 +0200

    druhy ukol jde

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'MainPage::index');
+$routes->get("zavody/(:any)", "MainPage::zavody/$1");
--- app/Controllers/MainPage.php
+++ app/Controllers/MainPage.php
@@ -23,5 +23,25 @@ class MainPage extends BaseController
             "pager" => $lokace->pager
         ];
         echo view("main_page", $data);
+
+    }
+    public function zavody($country)
+    {
+        $config = new MyConfig();
+        $perPage = $config->perPage;
+        
+        $lokaceZavodu = new RaceYear(); 
+        
+        $dataLokace = $lokaceZavodu->where('country', $country)->first();
+        
+        $dataZavod = $lokaceZavodu->select("race.default_name, COUNT(*) as pocet") ->join("race", "race.id = race_year.id_race", "inner")->where("race.country", $dataLokace->country)->groupBy("race.default_name")->orderBy("race.default_name", "asc")->paginate($perPage);
+    
+        $data = [
+            "lokace" => $dataZavod,
+            "pager"  => $lokaceZavodu->pager 
+        ];
+        
+        echo view("zavody", $data); 
     }
 }
+
